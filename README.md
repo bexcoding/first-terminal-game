ca9389b2a24a8f4eaa30627e7c8408e1771dd7da
Nova Amiko <beckhv2@gmail.com>
2023-04-04 21:44:09 -0800

    moved files from old repository to new location

---
+++ README.md
@@ -0,0 +1,29 @@
+# My First Python Terminal Game
+
+This is a game written for the command line using python. The original game was written as my first undirected project.
+
+### Premise and Background
+---
+
+I wrote this game at my lowest point in my nursing career while taking care of COVID patients in the ICU. The plot is that you have to make it through a day as a nurse while not getting in trouble with your boss or getting attacked by a drunk man. The scenarios played out the way that I felt at the time - that there was no way to make everyone happy or to predict what crazy things would happen in a single day. While things are much better for me now as I revise the code, it is funny to look back on the wacky scenarios that I invented for the game.
+
+This is one of my first programs that I wrote after I learned python. It is written in the style of the earliest computer games: text-based adventure. While there are cooler video games that can be created with graphics, text-based games allow beginning coders to circumnavigate the difficult code that is involved in modern video games and focus on logic - i.e., if you do this thing, some other thing will happen.
+
+I originally wrote the code for this game in May of 2022. This original code is labeled as 'original-game.py' and will remain unchanged from its original form. However, even weeks after writing the program, I learned of better ways of performing some of the tasks within the code. The most obvious example that catches my eye as I look at the original is that I didn't know about 'newline' characters yet. Every time I wanted a new line, I would add a 'print(" ")' statement which could be easily cleaned up by adding '\n' in the given string.
+
+The newer version written in February of 2023 is just one of the ways that I figured that the code could be re-written. This new version is labeled as 'updated-game.py'. Feel free to look at both versions to see how this game changed over time.
+
+
+### Changes
+---
+
+1. Replaced each instance of 'print(" ")' with "\n". This allows a newline without requiring a line of code because the newline character can be written right into the string.
+2. Added the if __name__ == "__main__" component at the bottom of the code. This allows automatic starting of the code if it's being run directly.
+3. Abstracted scenarios from the function running the scenarios. The original code constantly repeated several components including printing a message, asking for input, cleaning up the input, and checking which scenario to run afterwards. I removed the repitition and created a function to perform all of these tasks along with a helper function. All of the scenario information was put into 7 different lists.
+4. The game over screen retained the option for getting all of the answers to win but the cheat code phrase was removed. I felt that two different cheats weren't necessary.
+
+### Possible Improvements
+---
+
+1. The code ideally needs re-formatting to keep the lines of codes under 80 columns. This is mainly an issue in the scenario strings, which are long run-on strings. I would likely accomplish this via the multi-line string functionality with python. This uses three quote marks before and after the string. Then the string can be formatted so it doesn't run past the 80th column.
+2. I think that the functionality of the code can be maintained while combining the "message" string and the "user input" string. The two could be combined into one longer string that collects the user input. This would cut some lines of code and would simplify the solution.
---
+++ updated-game.py
@@ -0,0 +1,147 @@
+'''
+Title: Python Terminal Game, Updated
+Description: Initiates a text-based survival game from the command line.
+Developer: Alexander Beck
+Email: beckhv2@gmail.com
+Github: https://github.com/bexcoding
+
+\\......//\\......//\\......//\\......//\\......//\\......//\\......//\\......//
+ \\....//  \\....//  \\....//  \\....//  \\....//  \\....//  \\....//  \\....// 
+\ \\..// /\ \\..// /\ \\..// /\ \\..// /\ \\..// /\ \\..// /\ \\..// /\ \\..// /
+ \ \\// /  \ \\// /  \ \\// /  \ \\// /  \ \\// /  \ \\// /  \ \\// /  \ \\// / 
+> | || | <> | || | <> |                                     > | || | <> | || | <  
+> | || | <> | || | <> | ||                          ||   // > | || | <> | || | <
+ /  /\  \  /  /\  \  /  ||                          ||  //   /  /\  \  /  /\  \ 
+/  /  \  \/  /  \  \/   ||____       ___      ___   || //   /  /  \  \/  /  \  \
+  | <> | || | <> | ||   ||    \\   //   \\  //   \\ ||||    | | <> | || | <> | |
+  | <> | || | <> | ||   ||     || ||____|| ||       || \\   | | <> | || | <> | |
+   \  /  ||  \  /  ||   ||     || ||       ||       ||  \\  |  \  /  ||  \  /  | 
+\\  \/  //\\  \/  //\\  ||____//   \\___//  \\___// ||   \\ \\  \/  //\\  \/  //
+.\\    //..\\    //..\\                                      \\    //..\\    //.
+..\\  //....\\  //....\\  //....\\  //....\\  //....\\  //....\\  //....\\  //..
+...\\//......\\//......\\//......\\//......\\//......\\//......\\//......\\//...
+
+'''
+
+#helps run_scene choose whether to run next scenario, give win, or give loss
+def win_lose(scn, option):
+    if type(option) == list:
+        run_scene(option)
+    elif type(option) == tuple:
+        print(option[0])
+        if option[1] == "lose":
+            game_over_screen()
+        else:
+            game_win_screen()
+
+#given a list with scenario options, will exit, take the user to the next scenario, or display an error
+def run_scene(scenario):
+    print(scenario[0])
+    user_input = input(scenario[1])
+    user_input = (user_input.strip()).lower()
+    if user_input == "x":
+        exit()
+    elif user_input in scenario[2]:
+        win_lose(scenario, scenario[2][user_input])
+    else:
+        print("\nThere was an error. Please try again and check your spelling.")
+        run_scene(scenario)
+
+#displays the initial messages of the game to determine if the user wants to play the game or not
+def starting_screen():
+    print("You lift your head off your keyboard and realize you're still at work. Welcome to a day in the life of a nurse.")
+    init_input = input("Do you dare continue? If you would like to play, type 'yes'. Type 'no' to end the game: ")
+    if init_input.lower() == "yes":
+        pass
+    elif init_input.lower() == "no":
+        print("Game Over")
+        exit()
+    else:
+        print("\nThere was an error. Please try again and check your spelling.")
+        starting_screen()
+
+            
+#displays a losing game message and asks if they want to play again
+def game_over_screen():
+    print("""\n
+    {{{{{}  {{{{{}  {}  {}  {{{{{}    {{{{{}  {}  {}  {{{{{}  {{{{{}
+    {}      {}  {}  {}{}{}  {}        {}  {}  {}  {}  {}      {}  {}
+    {} {{}  {{{{{}  {}  {}  {{{{{}    {}  {}  {}  {}  {{{{{}  {{{{}
+    {}  {}  {}  {}  {}  {}  {}        {}  {}   {}{}   {}      {}  {}
+    {{{{{}  {}  {}  {}  {}  {{{{{}    {{{{{}    {}    {{{{{}  {}  {}
+   \n """)
+    user_input = input("Would you like to play again? If so, type 'yes'. If not, type 'no'. If you have already lost several times and you would like know the correct series of answers, type 'answers': ")
+    if user_input.lower() == "yes":
+        main()
+    elif user_input.lower() == "no":
+        print("Thanks for playing!")
+    elif user_input.lower() == "answers":
+        print("\nThe answers in order are: 'wait', 'stay', 'run', 'away', 'ball'. If you type these answers in this order, you should win the game.\n")
+        main()
+    else:
+        print("\nThere was an error. Please try again and check your spelling.")
+        game_over_screen()
+
+#displays the winning message
+def game_win_screen():
+    print("\nCongratulations. You won! You successfully completed the day as a nurse.")
+    print("""
+                 {{{{}   {{{{}  {}  {}
+                 {}  {}  {{     {}  {}
+                 {{{{}   {{{}    {}{}
+                 {}  {}  {{     {}  {}
+                 {{{{}   {{{{}  {}  {} Rx
+
+                                   [[[
+                 [[[[[[[[[[[[[[[[[[[[[     [[
+    <============[[[[[[[[[[[[[[[[[[[[[[[[[[[[
+                 [[[[[[[[[[[[[[[[[[[[[     [[
+                                   [[[    
+ 
+  """)
+    user_input = input("Would you like to play again? If so, type 'yes'. If not, type 'no': ")
+    if user_input.lower() == "yes":
+        main()
+    else:
+        print("Game Over")
+        exit()
+        
+#this is the list of scenarios. there are seven in total. they contain a prompt for the scenario, a message that asks what the user wants to do, and a
+#dictionary with the keywords and the results of using those keywords. they are listed in reverse order because
+#they refer to the next scene and the next scene has to exist before being referred to
+scene7 = ["\nAs you run away, the man's last dose of meth kicks in and he is now much faster than you. What do you do?",
+          "Type 'judo' to judo kick. Type 'ball' to curl into a ball on the floor: ",
+          {"judo": ("\nYou successfully land the judo kick and bust the man's jaw. Later in the day you get busted by the feds. You lost your license and are a convicted felon.", "lose"), "ball": ("\nYou curl into a ball on the floor and sustain minimal damage. The naked man is brought to jail. Now that there is extra space on the unit, prepare yourself to get your next patient. At least there's no way the new patient will be as crazy as that last guy, right? ..........", "win")}]
+
+scene6 = ["\nYou call security. Within minutes, ten people are holding the man down in the hallway. What do you do?",
+          "Type 'medicine' to go grab the medicine to sedate the man. Type 'hold' to help hold the man down with the others: ",
+          {"medicine": ("\nYou grab the medicine and give it to the man. In the rush, you had accidentally grabbed a paralytic, causing the man to stop breathing. You are sued for negligence.", "lose"), "hold": ("\nYou hold the man down with the other people while another nurse grabs the medicine. The naked man moves at the last second and the needle goes into your hand on accident. All of the medicine is given. You pass out and need close monitoring in the emergency room for several hours.", "lose")}]
+
+scene5 = ["\nYou run over toward the scene and yell at the naked man to get his attention. Now that you have his attention, what do you do?",
+          "Type 'away' to run away from the man. Type 'dive' to dive into a nearby medical closet that you see: ",
+          {"away": scene7, "dive": ("\nYou dive into a medical closet but a rack of oxygen tanks falls on you and crushes you.", "lose")}]
+
+scene4 = ["\nYou stay where you are to see what happens. You see another nurse go toward the yelling and suddenly, a naked, bloody man jumps out and grabs her. What do you do?",
+          "Type 'run' to run over to the other nurse and help her. Type 'call' to call security from the nearest phone: ",
+          {"run": scene5, "call": scene6}]
+
+scene3 = ["\nYou run toward the direction of the yelling. A naked, bloody man jumps out of nowhere and grabs you. What do you do?",
+          "Type 'get away' to try to get away. Type 'push' to push the man away: ",
+          {"get away":("\nYou try to get away. This sends the man into a rage. He attacks you and you need one week in intensive care.",
+                       "lose"), "push":("\nYou push the man away and he stumbles through a glass door. The manager and security arrive just in time to see this. You're going to jail.", "lose")}]
+
+scene2 = ["\nA naked man runs out of the room where you heard the screaming. There is blood running down his neck and a wild look in his eyes. What do you do?",
+          "Type 'help' if you want to go help the naked man. Type 'stay' if you want to stay where you are to see what happens next: ",
+          {"help":("\nWhen you try to stop his neck from bleeding, the man thinks you are trying to choke him and he returns the favor. The other nurses weren't able to help you in time.", "lose") , "stay": scene4}]
+
+scene1 = ["\nWhen you lift your head off the keyboard, you hear a scream down the hallway. What do you do?",
+              "Type 'wait' if you want to wait to see what happens. Type 'investigate' to go investigate the noise: ",
+              {"wait": scene2, "investigate": scene3}]
+
+#main game loop
+def main():
+    starting_screen()
+    run_scene(scene1)
+           
+if __name__ == "__main__":
+    main()
